1e68c7d71877bf0a34297437daa74d21f7b8e926
Zachar13y <zachar13y@gmail.com>
2017-10-31 17:00:04 -0700

    Cookies sold function console logged in sales html

---
+++ bid.js
@@ -0,0 +1,26 @@
+// function Car(year, make, model, color){
+
+//     this.year;
+//     this.make;
+//     this.model;
+//     this.color;
+
+//     this.bids = [];
+// }
+
+// Car.prototype.addbid = function(bid){
+//     this.bid.add(bid);
+// };
+
+// const bmw = new Car('1998', 'BMW', '323', 'Silver');
+// const honda = new Car('1993', 'Honda', 'Accord', 'White');
+
+
+// const button = document.createElement('button');
+//     button.textContent = ('Bid');
+//     button.onclick = function(){
+//         const name = prompt('Name:'); 
+//         const bidAmount = prompt('Enter bid amount.')
+//     }
+
+
--- sales.js
+++ sales.js
@@ -1,52 +1,75 @@
+'use strict';
 // array literal, literally the numbers in an array
-const locations = ['pdxAirport', 'pioneerSquare', 'Powells', 'stJohns', 'waterfront']
-const hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm'];
-
+const locations = ['pdxAirport', 'pioneerSquare', 'Powells', 'stJohns', 'waterfront'];
+const hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 
 function getRandomIntInclusive(min, max){
     min = Math.ceil(min);
     max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive
-}
-//object literal, literally the props given in an object
-    
-const  pdxAirport ={
-    calcSales: getRandomIntInclusive,
+    return Math.floor(Math.random() * (max - min + 1)) + min;}//The maximum is inclusive and the minimum is inclusive
+                                    
+const  pdx = {
+    name: 'PDX Airport',            //change const names!!!!!
     min: 23,
     max: 65,
-    avg: 6.3
-
+    avg: 6.3,
+    cookiesSold: hourlySales
+    
 };
 
-for (let i = 0; i < hours.length; i++){
-    console.log (pdxAirport.calcSales(pdxAirport.min, pdxAirport.max,))
+const Powells = {
+    min: 11,
+    max: 38,
+    avg: 3.7,
+    hourlySales: hourlySales
+};
+
+const stJohns = {
+    min: 20,
+    max: 38,
+    avg: 2.3,
+    hourlySales: hourlySales
+};
+
+const waterfront = {
+    min: 2,
+    max: 16,
+    avg: 4.6,
+    hourlySales: hourlySales
+};
+
+const pioneerSquare = {
+    min: 3,
+    max: 24,
+    avg: 1.2,
+    cookiesSold: hourlySales
+};
+
+function hourlySales(){
+    const salesPerHour = [];
+    for (let i = 0; i < hours.length; i++){
+        const hour = hours[i];
+        const numCustomers = getRandomIntInclusive(this.min, this.max);
+        const numCookies = Math.floor(numCustomers * this.avg);
+        const oneHour = {
+            hour: hour,
+            cookiesSold: numCookies
+        };
+        salesPerHour.push(oneHour);
+    }
+    this.salesPerHour = salesPerHour;
 }
+pdx.cookiesSold();
+console.log(pdx);
 
-
-    
-// };
-
-// const pioneerSquare = {
-//     min: 3,
-//     max: 24,
-//     avg: 1.2
-// };
-
-// const Powells = {
-//     min: 11,
-//     max: 38,
-//     avg: 3.7
-// };
-
-// const stJohns = {
-//     min: 20,
-//     max: 38,
-//     avg: 2.3
-// };
-
-// const waterfront = {
-//     min: 2,
-//     max: 16,
-//     avg: 4.6
-// };
+// for (let i = 0; i <= hours.length; i++){
+//     pdxAirport.hourlySales(pdxAirport.min, pdxAirport.max);
+//     Powells.hourlySales(Powells.min, Powells.max);
+//     stJohns.hourlySales(stJohns.min, stJohns.max);
+//     waterfront.hourlySales(stJohns.min, stJohns.max);
+//     pioneerSquare.hourlySales(pioneerSquare.min, pioneerSquare.max);
+//}
+//console.log(locations);
+console.log(locations);
+console.log(numCustomers);
 
